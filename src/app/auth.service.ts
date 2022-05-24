e2153b55982369eb48beeff0d691148a4fb98fb5
Mussab.Ayub <mussabayubawa1@gmail.com>
2022-05-24 06:07:06 -0700

    working on auth data and trying to get data from response in login.component.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,9 +7,11 @@ import {CallApiService} from "./call-api.service";
 export class AuthService {
   private _isAuthenticated:boolean;
   private _token:string|null;
-  constructor(private apiService:CallApiService) { 
+  private _user:{}|null|string;
+  constructor(private apiService:CallApiService) {
     this._token=localStorage.getItem('token');
     this._isAuthenticated=!!this._token
+    this._user=localStorage.getItem('user')
   }
   get isAuthenticated()
   {
@@ -19,8 +21,23 @@ export class AuthService {
   {
     this._isAuthenticated=status;
   }
+  get token()
+  { return this._token}
+  set token(token:string |null)
+  {
+    this._token=token;
+  }
+  get user()
+  {
+    return this._user;
+  }
+  set user(user:{}|null)
+  {
+    this._user=user;
+  }
   private _loginURL:string='mobileLogin'
   loginUser(data:{}) {
+    // return this.apiService.postData(this._loginURL, data)
     return this.apiService.postData(this._loginURL, data)
   }
 
